6bfe79d5396b73ac5375d96968a9a6cb8a7a5c0a
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-10-12 10:58:02 -0500

    Works for R4B but not R5

--- start-server.sh
+++ start-server.sh
@@ -2,17 +2,22 @@
 
 LOCAL_PORT=8080
 CONTAINER_NAME=hapi-fhir
-CONFIG_FILENAME=app.yaml
+CONFIG_FILENAME=r4b.yaml
+FHIR_VERSION=R4B
 DBDIR=db
+
 
 # Create the database working directory and open the priveleges up
 mkdir -p $DBDIR
 chmod 777 $DBDIR
 
-docker run \
+# Right now, I can only get the R4B version to work (not counting 
+# vanilla R4). So, just leave things as they are until the issues with R5 are
+# worked out. 
+echo docker run \
     --rm \
     -p $LOCAL_PORT:8080 \
     -v $PWD/cfg:/configs \
-    -v $PWD/$DBDIR:/hapi \
+    -v $PWD/$DBDIR:/app/database \
     -e "--spring.config.location=file:///configs/$CONFIG_FILENAME" \
     hapiproject/hapi:latest -n $CONTAINER_NAME
